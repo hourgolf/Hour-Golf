f21bd770451945e78eae1f2467c70d6232d6c5ac
hourgolf <matt@multifresh.com>
2026-04-13 05:53:13 -0700

    Update Badge.js

--- components/ui/Badge.js
+++ components/ui/Badge.js
@@ -1,10 +1,11 @@
 import { TIER_COLORS } from "../../lib/constants";
 
 export default function Badge({ tier, style, className = "" }) {
+  const colors = TIER_COLORS[tier] || { bg: "#D1DFCB", text: "#35443B" };
   return (
     <span
       className={`badge ${className}`}
-      style={{ background: TIER_COLORS[tier] || "#888", ...style }}
+      style={{ background: colors.bg, color: colors.text, ...style }}
     >
       {tier}
     </span>
